d823753dd0ff33e43608127085bc5b250e9f4534
Jose <josemelendezramos123@gmail.com>
2025-04-02 18:52:01 -0500

    Roles: Inicializados
    Laravel spatie instalado y configurado en el modelo usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 // Ensure the Perfil class exists in the App\Models namespace
 use App\Models\Perfil;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
